Fixing model tests Deleting scaling responsibility from models Fixed TimeSeries computing data shape to have (Batch, Features, Steps)

src/sdg_core_lib/dataset/datasets.py
@@ -92,7 +92,7 @@ class Table(Dataset):
         return Table(columns, processor, pk_indexes)
 
     def clone(self, data: np.ndarray) -> 'Table':
-        if self.get_computing_shape()[-1] != data.shape[-1]:
+        if self.get_computing_shape()[1] != data.shape[1]:
             raise ValueError("Data does not match table shape on column axis")
         n_rows = data.shape[0]
         new_columns = []
@@ -218,13 +218,13 @@ class TimeSeries(Table):
 
     def clone(self, data: np.ndarray) -> 'TimeSeries':
         if len(data.shape) == 3:
-            # collapsing time steps
-            time_steps = data.shape[1]
-            data = data.reshape(-1, data.shape[2])
+            time_steps = data.shape[-1]
+            data = data.transpose(0, 2, 1).reshape(-1, data.shape[1])
         else:
             raise ValueError("Data must be a 3D array")
         sub_table = super().clone(data)
-        # Generate new Group Index and update pk_column
+        # Generate new Group Index and update the column
+        # TODO: Currently we support only "integer" group indexes
         new_group_index = np.repeat(np.arange(data.shape[0]/time_steps, dtype="int"), repeats=time_steps).reshape(-1, 1)
         new_group_col = type(sub_table.columns[self.group_index])(
             sub_table.columns[self.group_index].name,
@@ -256,7 +256,7 @@ class TimeSeries(Table):
     def get_computing_data(self) -> np.ndarray:
         time_steps = self._get_experiment_length()
         data = np.hstack([col.get_data() for col in self._get_computing_column()])
-        return data.reshape(-1, time_steps, data.shape[1])
+        return data.reshape(-1, time_steps, data.shape[1]).transpose(0, 2, 1)
 
     def preprocess(self) -> "TimeSeries":
         new_cols = self.processor.process(self.columns)

src/sdg_core_lib/test/data_generator/models/keras/implementation/test_TabularVAE.py
@@ -1,10 +1,8 @@
-import numpy as np
 import pytest
 import os
 import shutil
-from sklearn.preprocessing import StandardScaler
 
-from sdg_core_lib.NumericDataset import NumericDataset
+from sdg_core_lib.dataset.datasets import Table
 from sdg_core_lib.data_generator.models.TrainingInfo import TrainingInfo
 from sdg_core_lib.data_generator.models.keras.VAE import VAE
 from sdg_core_lib.data_generator.models.keras.implementation.TabularVAE import (
@@ -14,7 +12,7 @@ from sdg_core_lib.data_generator.models.keras.implementation.TabularVAE import (
 
 @pytest.fixture()
 def data():
-    return NumericDataset(
+    return Table.from_json(
         [
             {
                 "column_name": "A",
@@ -22,7 +20,7 @@ def data():
                 "column_datatype": "float64",
                 "column_data": [1.0, 2.0, 3.0, 4.0, 5.0],
             }
-        ]
+        ], None
     )
 
 
@@ -55,15 +53,6 @@ def test_instantiate(model_data_no_load):
     assert model.input_shape == (13,)
     assert model._epochs == 1
     assert type(model._model) is VAE
-    assert model._scaler is None
-
-
-def test_preprocess(model_data_no_load, data):
-    model = TabularVAE(**model_data_no_load)
-    assert model._scaler is None
-    scaled_data = model._pre_process(data)
-    assert model._scaler is not None and type(model._scaler) is StandardScaler
-    assert type(scaled_data) is np.ndarray
 
 
 def test_self_description(model_data_no_load):
@@ -93,23 +82,20 @@ def test_save(model_data_no_load):
     model.save(model_path)
     assert os.path.isfile(os.path.join(model_path, "encoder.keras"))
     assert os.path.isfile(os.path.join(model_path, "decoder.keras"))
-    assert os.path.isfile(os.path.join(model_path, "scaler.skops"))
     shutil.rmtree(model_path)
 
 
 def test_train_wrong(model_data_no_load, data):
     model = TabularVAE(**model_data_no_load)
     with pytest.raises(ValueError) as exception_info:
-        model.train(data)
+        model.train(data.get_computing_data())
     assert exception_info.type is ValueError
 
 
 def test_train_correct(model_data_correct_train, data):
     model = TabularVAE(**model_data_correct_train)
     assert model.training_info is None
-    assert model._scaler is None
-    model.train(data)
-    assert type(model._scaler) is StandardScaler
+    model.train(data.get_computing_data())
     assert type(model.training_info) is TrainingInfo
 
 