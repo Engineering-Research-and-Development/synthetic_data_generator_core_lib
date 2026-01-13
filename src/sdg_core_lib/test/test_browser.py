08a54c2dd6fa3464f237e533b001a2f66aac09c0
emiliocimino <emilio.cimino@outlook.it>
2026-01-13 17:04:15 +0100

    Fixing tests

--- src/sdg_core_lib/test/test_browser.py
+++ src/sdg_core_lib/test/test_browser.py
@@ -3,6 +3,7 @@ from sdg_core_lib.browser import browse_algorithms, browse_functions
 
 def test_browse_algorithms():
     for desc in browse_algorithms():
+        print(desc)
         assert desc is not None
 
 
